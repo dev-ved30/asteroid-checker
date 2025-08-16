3b95bba98648fc45a14f1c7c247ddaeda0c49f23
ved <vedshah.email@gmail.com>
2025-08-16 00:11:10 -0500

    Basic implementation working on single core

--- AsteroidChecker.py
+++ AsteroidChecker.py
@@ -1,23 +1,38 @@
+import os
+os.environ["OMP_NUM_THREADS"] = "1"
+
 import kete
 import psycopg2
 
+import numpy as np
 import astropy.units as u
 
 from astropy.coordinates import SkyCoord, match_coordinates_sky
 from astropy.io import fits
 from astropy.wcs import WCS
+from memory_profiler import profile
 
 from AsteroidDB import db_params
 
 default_radius_threshold = 30 * u.arcsec
+chip_fov_radius = 1 * u.deg
+
+
+import psutil
+
+proc = psutil.Process(os.getpid())
+
+print(f"\nOS‑level threads: {proc.num_threads()}")
 
 class AsteroidChecker:
 
+    #@profile
     def __init__(self):
         
         # Load the database table and convert to states
         self.load_states_from_db()
     
+    #@profile
     def load_states_from_db(self, db_params=db_params):
 
         try:
@@ -45,42 +60,49 @@ class AsteroidChecker:
         except Exception as e:
             print("Error connecting to database:", e)
 
+    #@profile
     def get_asteroid_list(self, frame_wcs, time_jd):
 
-        earth = kete.spice.get_state("Earth", time_jd)
+        def _kete_state_ra_dec(state, sun2earth):
+            """
+            Private method to wrap computing the coordinates of a single state
+            """
 
-        fov = kete.fov.RectangleFOV.from_wcs(frame_wcs, earth)
+            # change the coordinate frame from heliocentric to geocentric
+            # then compute the geocentric coordinate
+            obj_earth_pos = state.pos - sun2earth 
+            state_vec_equitorial = obj_earth_pos.change_frame(kete.vector.Frames.Equatorial)
+            return state_vec_equitorial.ra, state_vec_equitorial.dec
 
-        # Update the states based on the time. 
-        self.mpc_states = kete.propagate_n_body(self.mpc_states, time_jd)
 
-        # Check the FOV for asteroids.
-        vis = kete.fov_state_check(self.mpc_states, [fov], dt_limit=0, include_asteroids=True)
+        sun2earth = kete.spice.get_state("Earth", time_jd)
 
-        ra_list = []
-        dec_list = []
-        designation_list = []
+        fov = kete.fov.RectangleFOV.from_wcs(frame_wcs, sun2earth)
+        fov_center_ra, fov_center_dec = fov.pointing.as_equatorial.ra, fov.pointing.as_equatorial.dec
+        target_skycoord =  SkyCoord(fov_center_ra, fov_center_dec, unit="deg")
 
-        if len(vis) > 0:
+        print(fov_center_ra, fov_center_dec)
 
-            vis = vis[0]
+        # Update the states based on the time using the two body approximation
+        new_mpc_states_2body = kete.propagate_two_body(self.mpc_states, time_jd)
+        ras, decs = np.array([_kete_state_ra_dec(state, sun2earth.pos) for state in new_mpc_states_2body]).T
 
-            for idx in range(len(vis)):
-                
-                vec = vis.obs_vecs[idx]
+        # Only get MPC matches that are withing 2*survey fov radius 
+        mpc_skycoords_approx = SkyCoord(ras, decs, unit="deg")
+        idxs = np.where(target_skycoord.separation(mpc_skycoords_approx) < chip_fov_radius)[0]
 
-                designation_list.append(vis[idx].desig)
-                ra_list.append(vec.as_equatorial.ra)
-                dec_list.append(vec.as_equatorial.dec)
+        # Now use the full n-body solution for all of these objects
+        objs_win_first_cut = np.array(self.mpc_states)[idxs]
+        print(f"Found {len(objs_win_first_cut)} w/in {2 * chip_fov_radius}, running full n-body on those...")
 
-            asteroid_sky_coords = SkyCoord(ra=ra_list, dec=dec_list, unit='deg')
+        states = kete.propagate_n_body(objs_win_first_cut, time_jd)
+        ras, decs = np.array([_kete_state_ra_dec(state, sun2earth.pos) for state in states]).T
+        mpc_skycoords = SkyCoord(ras, decs, unit="deg")
+        designations_list = [s.desig for s in objs_win_first_cut]
 
-        else:
+        return mpc_skycoords, designations_list
 
-            asteroid_sky_coords = []
-
-        return asteroid_sky_coords, designation_list
-
+    #@profile
     def cross_match_sources_with_asteroids(self, asteroid_sky_coords, asteroid_designations, sources_sky_coords, radius_threshold=default_radius_threshold):
 
         matched_asteroids = []
@@ -117,8 +139,19 @@ if __name__=="__main__":
     # Timing the code 
     start_time = time.perf_counter()
 
-    jd = kete.Time.from_ymd(2025, 2, 23).jd
+    jd = 2460730.0 #kete.Time.from_ymd(2025, 2, 23).jd
     asteroid_sky_coords, designations = ac.get_asteroid_list(frame_wcs, jd)
+
+    print(asteroid_sky_coords)
+
+    new_ra  = 150.123456   # degrees
+    new_dec =   26.54321   # degrees
+
+    # Update only CRVAL (leaving CRPIX unchanged)
+    frame_wcs.wcs.crval = [new_ra, new_dec]
+    asteroid_sky_coords, designations = ac.get_asteroid_list(frame_wcs, jd)
+
+
     print(asteroid_sky_coords)
 
     end_time = time.perf_counter()
@@ -130,6 +163,11 @@ if __name__=="__main__":
 
 
 
+    mem = proc.memory_info()
+    print(f"RSS: {mem.rss / (1024**2):.2f} MiB")
+    print(f"VMS: {mem.vms / (1024**2):.2f} MiB")
+
+    print("CPU % (1s):", proc.cpu_percent(interval=1))  # ≤ 100%
 
 
 
